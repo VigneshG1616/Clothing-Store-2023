a95989a35c3151ce5fa3736bac8a58f1650f597d
VigneshG1616 <vg.thefire@gmail.com>
2023-03-18 22:26:02 +0530

    added exta features

--- src/components/CartDropdown/CartDropdown.component.jsx
+++ src/components/CartDropdown/CartDropdown.component.jsx
@@ -6,17 +6,24 @@ import Button from "../Button/Button.component";
 import "./CartDropdown.styles.scss";
 
 const CartDropdown = () => {
-  const {cartItems} = useContext(CartContext);
+  const {cartItems, setIsDropdownOn} = useContext(CartContext);
   const navigateToCheckout = useNavigate();
   const goToCheckOutPageHandler = () => navigateToCheckout("/checkout");
-  return (
+  const closeCartDropdownHandler = () => setIsDropdownOn(false);
+  return ( 
     <div className="cart-dropdown-container">
-      
-      <div className="cart-items">
+      <div className="close-icon" onClick={closeCartDropdownHandler}>X</div>
+      {cartItems.length?(<><div className="cart-items">
         {cartItems.map((cartItem)=> <CartItems key={cartItem.id} {...cartItem} />)}
-        
       </div>
-    <Button onClick={goToCheckOutPageHandler} >Checkout</Button>
+    <Button onClick={goToCheckOutPageHandler} >Checkout</Button></>):(<div className="empty-message">Cart is Empty</div>)
+
+
+      }
+      
+   
+      
+      
     </div>
   );
 };
